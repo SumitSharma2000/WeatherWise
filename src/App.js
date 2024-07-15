ab5f4fb5d435969a49f3c8bd33abf5c2f08df70f
Sumit Sharma <sumitsharma7624@gmail.com>
2024-07-16 04:38:02 +0530

    added more data

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
         .then(result => {
           setWeather(result);
           setQuery('');
-          console.log(result);
         });
     }
   }
@@ -35,7 +34,7 @@ function App() {
 
   return (
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
-      <main>
+      <main className="main-content">
         <div className="search-box">
           <input 
             type="text"
@@ -45,13 +44,12 @@ function App() {
             value={query}
             onKeyPress={search}
           />
-          <div className="app-name">Weather App : WeatherWise by Sumit Sharma </div>
-
+          <div className="app-name">WeatherWise: Weather App by Sumit Sharma</div>
         </div>
         {(typeof weather.main != "undefined") ? (
-        <div>
+        <div className="weather-info">
           <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="location">{weather.name},{weather.sys.country}</div>
             <div className="date">{dateBuilder(new Date())}</div>
           </div>
           <div className="weather-box">
@@ -60,16 +58,22 @@ function App() {
             </div>
             <div className="weather">{weather.weather[0].main}</div>
           </div>
-            <div className='extra-info'>
+          <div class="extra-info-container">
+            <div className='extra-info '>
             <div>Max Temp: {weather.main.temp_max}°c</div>
             <div>Min Temp: {weather.main.temp_min}°c</div>
+            <div>Feels Like: {weather.main.feels_like}°c</div>
             <div>Wind Speed: {weather.wind.speed} m/s</div>
+            <div>Pressure: {weather.main.pressure}°c</div>
+            <div>Humidity: {weather.main.humidity}°c</div>
             <div>Sunrise: {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()}</div>
             <div>Sunset: {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}</div>
             <div>Latitude: {weather.coord.lat}</div>
             <div>Longitude: {weather.coord.lon}</div>
             <div>Timezone: {weather.timezone}</div>
             <div>Visibility: {weather.visibility} meters</div>
+
+            </div>
             </div>
         </div>
         ) : ('')}
